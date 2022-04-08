854b9acbcaffc48c7260954f762f29e142b25573
Hassan Shaikh <shaikh.hassan01@outlook.com>
2022-04-08 07:42:05 -0400

    Refactored the code and now have two functions, One to get the value of form and other to create the element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function (event) {
+var TaskFormHandler = function (event) {
   event.preventDefault();
   //console.log(event);
 
@@ -9,6 +9,17 @@ var createTaskHandler = function (event) {
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  //package up data as obj
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput,
+  };
+
+  //send it as an argument to createTaskEl
+  createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function (taskDataObj) {
   //create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
@@ -20,16 +31,16 @@ var createTaskHandler = function (event) {
   //add HTML content to div
   taskInfoEl.innerHTML =
     "<h3 class='task-name'>" +
-    taskNameInput +
+    taskDataObj.name +
     "</h3><span class='task-type'>" +
-    taskTypeInput +
+    taskDataObj.type +
     "</span>";
 
   listItemEl.appendChild(taskInfoEl);
 
   //add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
-  console.dir(listItemEl);
+  //console.dir(listItemEl);
 };
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", TaskFormHandler);
